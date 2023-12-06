355346d4a29acc6149b1738bfc1eda1267b8e114
JosephBabs <bajos3d@gmail.com>
2023-12-06 13:32:24 +0100

    first commit

---
+++ components/HeaderHome.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="inner-banner">
+        <div class="container">
+            <ul class="list-unstyled thm-breadcrumb">
+                <!-- <li><a href="/">Accueil</a></li> -->
+                <!-- <li class="active"><a href="#">{{ title }}</a></li> -->
+            </ul><!-- /.list-unstyled -->
+            <h2 class="inner-banne__title" style="color:#fff; font-size: 40px; ; 
+">{{ title }}</h2>
+            <br>
+            <br>
+            <!-- /.inner-banner__title -->
+        </div><!-- /.container -->
+    </section>
+  </template>
+  
+  <script>
+      export default {
+        name: "HeaderHome",
+        props: {
+          title: {
+            type: String
+          }
+        }
+      }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
